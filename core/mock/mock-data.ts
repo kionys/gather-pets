71aefb2a419930f12292495cb5bf2d3e2484022a
kionys <kiwon7271@naver.com>
2024-10-14 22:46:32 +0900

    chore: mock-data added

---
+++ core/mock/mock-data.ts
@@ -0,0 +1,93 @@
+export const dogPosts = [
+  {
+    id: 1,
+    title: "우리 강아지의 첫 산책",
+    content: "오늘 우리 강아지와 함께 처음으로 산책을 나갔어요! 너무 신나서 여기저기를 뛰어다니더군요.",
+    author: "홍길동",
+    date: "2024-10-10",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/react-twitter-133b2.appspot.com/o/SP1BIx2hzJaqDXYmZovXufHZO5H2%2F7db27399-5548-4b59-b406-10d3aeed164b?alt=media&token=898d2fb2-6938-45e4-a8e5-5c169dfe64c7", // Pexels
+  },
+  {
+    id: 2,
+    title: "특별한 간식 만들기",
+    content: "홈메이드 간식을 만들어봤어요! 반려견이 정말 맛있게 먹더라고요.",
+    author: "김철수",
+    date: "2024-10-11",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/react-twitter-133b2.appspot.com/o/SP1BIx2hzJaqDXYmZovXufHZO5H2%2F7db27399-5548-4b59-b406-10d3aeed164b?alt=media&token=898d2fb2-6938-45e4-a8e5-5c169dfe64c7", // Pexels
+  },
+  {
+    id: 3,
+    title: "강아지 훈련의 중요성",
+    content: "훈련이 정말 중요하다는 것을 깨달았어요. 우리 강아지가 점점 더 똑똑해지고 있어요!",
+    author: "이영희",
+    date: "2024-10-12",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/react-twitter-133b2.appspot.com/o/SP1BIx2hzJaqDXYmZovXufHZO5H2%2F7db27399-5548-4b59-b406-10d3aeed164b?alt=media&token=898d2fb2-6938-45e4-a8e5-5c169dfe64c7", // Pexels
+  },
+  {
+    id: 4,
+    title: "반려견과 함께하는 여행",
+    content: "이번 주말에 반려견과 함께 바다로 여행을 갔어요. 정말 행복한 시간이었습니다!",
+    author: "박지민",
+    date: "2024-10-13",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/react-twitter-133b2.appspot.com/o/SP1BIx2hzJaqDXYmZovXufHZO5H2%2F7db27399-5548-4b59-b406-10d3aeed164b?alt=media&token=898d2fb2-6938-45e4-a8e5-5c169dfe64c7", // Pexels
+  },
+  {
+    id: 5,
+    title: "새로운 장난감",
+    content: "새로 산 장난감이 너무 마음에 들어요! 하루 종일 물고 다니네요.",
+    author: "최준호",
+    date: "2024-10-14",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/react-twitter-133b2.appspot.com/o/SP1BIx2hzJaqDXYmZovXufHZO5H2%2F7db27399-5548-4b59-b406-10d3aeed164b?alt=media&token=898d2fb2-6938-45e4-a8e5-5c169dfe64c7", // Pexels
+  },
+  {
+    id: 6,
+    title: "반려견과의 특별한 순간",
+    content: "오늘 아침, 강아지가 제 곁에 와서 졸고 있는 모습이 정말 귀여웠어요.",
+    author: "오민지",
+    date: "2024-10-15",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/react-twitter-133b2.appspot.com/o/SP1BIx2hzJaqDXYmZovXufHZO5H2%2F7db27399-5548-4b59-b406-10d3aeed164b?alt=media&token=898d2fb2-6938-45e4-a8e5-5c169dfe64c7", // Pexels
+  },
+  {
+    id: 7,
+    title: "반려견의 건강 관리",
+    content: "정기적인 검진과 운동이 얼마나 중요한지 다시 한번 느꼈습니다.",
+    author: "임재훈",
+    date: "2024-10-16",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/react-twitter-133b2.appspot.com/o/SP1BIx2hzJaqDXYmZovXufHZO5H2%2F7db27399-5548-4b59-b406-10d3aeed164b?alt=media&token=898d2fb2-6938-45e4-a8e5-5c169dfe64c7", // Pexels
+  },
+  {
+    id: 8,
+    title: "귀여운 복종 훈련",
+    content: "우리 강아지가 이제 앉아와 손을 잘 해요. 정말 뿌듯해요!",
+    author: "이수진",
+    date: "2024-10-17",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/react-twitter-133b2.appspot.com/o/SP1BIx2hzJaqDXYmZovXufHZO5H2%2F7db27399-5548-4b59-b406-10d3aeed164b?alt=media&token=898d2fb2-6938-45e4-a8e5-5c169dfe64c7", // Pexels
+  },
+  {
+    id: 9,
+    title: "반려견과의 일상",
+    content: "반려견과 함께하는 매일매일이 정말 소중해요. 사랑합니다!",
+    author: "강지혜",
+    date: "2024-10-18",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/react-twitter-133b2.appspot.com/o/SP1BIx2hzJaqDXYmZovXufHZO5H2%2F7db27399-5548-4b59-b406-10d3aeed164b?alt=media&token=898d2fb2-6938-45e4-a8e5-5c169dfe64c7", // Pexels
+  },
+  {
+    id: 10,
+    title: "소중한 반려견",
+    content: "우리 집의 귀여운 막내! 언제나 내 곁에서 나를 행복하게 해줘요.",
+    author: "정명수",
+    date: "2024-10-19",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/react-twitter-133b2.appspot.com/o/SP1BIx2hzJaqDXYmZovXufHZO5H2%2F7db27399-5548-4b59-b406-10d3aeed164b?alt=media&token=898d2fb2-6938-45e4-a8e5-5c169dfe64c7", // Pexels
+  },
+];
+export const avatars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
