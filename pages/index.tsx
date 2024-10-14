bf328ded5008c97c17361021843332f2e7b57f0b
kionys <kiwon7271@naver.com>
2024-10-14 18:29:03 +0900

    test: fetcher test code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
 import localFont from "next/font/local";
+import Image from "next/image";
+import { useEffect } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -13,6 +14,16 @@ const geistMono = localFont({
 });
 
 export default function Home() {
+   
+  useEffect(() => {
+  fetch("/api/hello")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data); // { name: "John Doe" } 출력
+    })
+    .catch((error) => console.error("Error fetching data:", error));
+}, []);
+
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
