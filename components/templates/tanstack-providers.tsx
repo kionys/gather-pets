80a9e5efe58724b97ce465885578d3a108b6813d
kionys <kiwon7271@naver.com>
2024-10-23 23:30:07 +0900

    add: tanstack-providers 추가

---
+++ components/templates/tanstack-providers.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+interface IPropsTanStackProvider {
+  children: React.ReactNode;
+}
+
+export const TanStackProvider = ({ children }: IPropsTanStackProvider) => {
+  // const [queryClient] = useState(() => new QueryClient({}));
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 5, // 5분 동안 데이터 신선 유지
+      },
+    },
+  });
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+};
