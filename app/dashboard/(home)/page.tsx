ed508370d5428a3c28765416e546b2b0904ba45f
kionys <kiwon7271@naver.com>
2024-10-23 23:37:19 +0900

    update: auth session getuserid 적용

--- app/dashboard/(home)/page.tsx
+++ app/dashboard/(home)/page.tsx
@@ -1,10 +1,12 @@
-import { Suspense } from "react";
+import Posts from "@/components/templates/posts";
+import { getUserId } from "@/lib/utils";
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+  const userId = await getUserId();
   return (
     <main className="flex w-full flex-grow">
       <div className="flex flex-col flex-1 gap-y-8 max-w-lg mx-auto pb-20">
-        <Suspense fallback={<>loading...</>}>{/* <Posts/> */}ss</Suspense>
+        <Posts userId={userId} />
       </div>
     </main>
   );
