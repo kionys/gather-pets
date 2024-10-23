dc43d735cdf0f3347d7f2da7aa8eafd1d0ea9227
kionys <kiwon7271@naver.com>
2024-10-23 23:36:04 +0900

    update: auth session for server component

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { auth } from "@/auth";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getUserId = async () => {
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  if (!userId) {
+    throw new Error("이 기능을 사용하려면 로그인해야 합니다");
+  }
+
+  return userId;
+};
